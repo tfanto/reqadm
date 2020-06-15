40f35b9b973dfad3fc6bb3843ee72363299def0c
Thomas Fanto <thomas@fanto.se>
2020-06-15 06:37:06 +0200

    i18n

--- src/main/java/pdmf/ui/Topic.java
+++ src/main/java/pdmf/ui/Topic.java
@@ -18,7 +18,8 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
-import pdmf.model.Cst;
+import pdmf.Main;
+import pdmf.model.Cst2;
 import pdmf.model.ProductKey;
 import pdmf.model.TopicKey;
 import pdmf.model.TopicRec;
@@ -69,7 +70,8 @@ public class Topic extends Dialog {
 	 */
 	public Topic(Shell parent, int style) {
 		super(parent, style);
-		setText("[" + Cst.TOPIC + "]");
+		String TOPIC = Main.cst(Cst2.TOPIC);
+		setText("[" + TOPIC + "]");
 	}
 
 	/**
@@ -98,16 +100,18 @@ public class Topic extends Dialog {
 		shell = new Shell(getParent(), getStyle());
 
 		shell.setSize(574, 600);
-		shell.setText(getText() + " " + mode + " "  + currentUser.getCurrentTenant().description);
+		shell.setText(getText() + " " + mode + " " + currentUser.getCurrentTenant().description);
 		shell.setLayout(null);
 
 		lblTopic = new Label(shell, SWT.NONE);
-		lblTopic.setText(Cst.TOPIC);
+		String TOPIC = Main.cst(Cst2.TOPIC);
+		lblTopic.setText(TOPIC);
 		lblTopic.setBounds(167, 5, 137, 25);
 
 		Label lblDescription = new Label(shell, SWT.NONE);
 		lblDescription.setBounds(10, 170, 151, 25);
-		lblDescription.setText(Cst.DESCRIPTION);
+		String DESCRIPTION = Main.cst(Cst2.DESCRIPTION);
+		lblDescription.setText(DESCRIPTION);
 
 		product = new Label(shell, SWT.BORDER);
 		product.setBounds(10, 33, 151, 25);
@@ -130,18 +134,21 @@ public class Topic extends Dialog {
 
 				String wrkTopicName = topic.getText();
 				if (wrkTopicName == null || wrkTopicName.trim().length() < 1) {
-					lblInfo.setText(Cst.NO_TOPIC_SELECTED);
+					String NO_TOPIC_SELECTED = Main.cst(Cst2.NO_TOPIC_SELECTED);
+					lblInfo.setText(NO_TOPIC_SELECTED);
 					return;
 				}
 
 				TopicKey key = new TopicKey(tenantId, version, wrkProductName, wrkTopicName);
 				if (processService.isDeleteMarked(key)) {
-					lblInfo.setText(Cst.ALREADY_DELETE_NO_ACTION);
+					String ALREADY_DELETE_NO_ACTION = Main.cst(Cst2.ALREADY_DELETE_NO_ACTION);
+					lblInfo.setText(ALREADY_DELETE_NO_ACTION);
 					return;
 				}
 
 				if (ProductService.isLocked(tenantId, version, wrkProductName)) {
-					lblInfo.setText(Cst.VERSION_LOCKED);
+					String VERSION_LOCKED = Main.cst(Cst2.VERSION_LOCKED);
+					lblInfo.setText(VERSION_LOCKED);
 					return;
 				}
 
@@ -149,7 +156,8 @@ public class Topic extends Dialog {
 
 				if (mode.equals(NEW_REG_MODE)) {
 					if (rec != null) {
-						lblInfo.setText(Cst.ALREADY_EXISTS);
+						String ALREADY_EXISTS = Main.cst(Cst2.ALREADY_EXISTS);
+						lblInfo.setText(ALREADY_EXISTS);
 						return;
 					}
 				}
@@ -173,12 +181,14 @@ public class Topic extends Dialog {
 					result = 1;
 					shell.dispose();
 				} catch (RecordChangedByAnotherUser rc) {
-					lblInfo.setText(Cst.RECORD_CHANGED_BY_ANOTHER_USER);
+					String RECORD_CHANGED_BY_ANOTHER_USER = Main.cst(Cst2.RECORD_CHANGED_BY_ANOTHER_USER);
+					lblInfo.setText(RECORD_CHANGED_BY_ANOTHER_USER);
 				}
 			}
 
 		});
-		btnStore.setText(Cst.STORE);
+		String STORE = Main.cst(Cst2.STORE);
+		btnStore.setText(STORE);
 
 		btnRemove = new Button(shell, SWT.NONE);
 		btnRemove.setBounds(458, 60, 80, 25);
@@ -191,18 +201,21 @@ public class Topic extends Dialog {
 				String productName = (String) product.getData();
 				String topicName = topic.getText();
 				if (topicName.trim().length() < 1) {
-					lblInfo.setText(Cst.NO_TOPIC_SELECTED);
+					String NO_TOPIC_SELECTED = Main.cst(Cst2.NO_TOPIC_SELECTED);
+					lblInfo.setText(NO_TOPIC_SELECTED);
 					return;
 				}
 
 				TopicKey key = new TopicKey(tenantId, version, productName, topicName);
 				if (processService.isDeleteMarked(key)) {
-					lblInfo.setText(Cst.ALREADY_DELETE_NO_ACTION);
+					String ALREADY_DELETE_NO_ACTION = Main.cst(Cst2.ALREADY_DELETE_NO_ACTION);
+					lblInfo.setText(ALREADY_DELETE_NO_ACTION);
 					return;
 				}
 
 				if (ProductService.isLocked(tenantId, version, productName)) {
-					lblInfo.setText(Cst.VERSION_LOCKED);
+					String VERSION_LOCKED = Main.cst(Cst2.VERSION_LOCKED);
+					lblInfo.setText(VERSION_LOCKED);
 					return;
 				}
 
@@ -217,7 +230,8 @@ public class Topic extends Dialog {
 				}
 			}
 		});
-		btnRemove.setText(Cst.REMOVE);
+		String REMOVE = Main.cst(Cst2.REMOVE);
+		btnRemove.setText(REMOVE);
 
 		lblInfo = new Label(shell, SWT.BORDER | SWT.SHADOW_IN);
 		lblInfo.setBounds(10, 499, 482, 25);
@@ -233,7 +247,8 @@ public class Topic extends Dialog {
 		shortDescription.setBounds(10, 91, 528, 73);
 
 		lblShortDescription = new Label(shell, SWT.NONE);
-		lblShortDescription.setText(Cst.DESCRIPTION_SHORT);
+		String DESCRIPTION_SHORT = Main.cst(Cst2.DESCRIPTION_SHORT);
+		lblShortDescription.setText(DESCRIPTION_SHORT);
 		lblShortDescription.setBounds(10, 65, 151, 25);
 
 		String tenantId = currentUser.getCurrentTenant().key.tenantid;
@@ -279,20 +294,25 @@ public class Topic extends Dialog {
 
 	}
 
-	private void handleInfo(Instant createDate, String createUser, Instant changeDate, String chgusr,
-			Instant deleteDate, String deleteUser, Integer createdInVersion) {
+	private void handleInfo(Instant createDate, String createUser, Instant changeDate, String chgusr, Instant deleteDate, String deleteUser, Integer createdInVersion) {
+
+		String CREATED = Main.cst(Cst2.CREATED);
+		String CHANGED = Main.cst(Cst2.CHANGED);
+		String REMOVED = Main.cst(Cst2.REMOVED);
+		String BY = Main.cst(Cst2.BY);
+		String IN_VERSION = Main.cst(Cst2.IN_VERSION);
 
 		LocalDate created = LocalDateTime.ofInstant(createDate, ZoneOffset.UTC).toLocalDate();
-		crtDat.setText(Cst.CREATED + created.toString() + Cst.BY + createUser + Cst.IN_VERSION + createdInVersion);
+		crtDat.setText(CREATED + created.toString() + BY + createUser + IN_VERSION + createdInVersion);
 
 		chgDat.setText("");
 		if (changeDate != null && chgusr != null) {
 			LocalDate changed = LocalDateTime.ofInstant(changeDate, ZoneOffset.UTC).toLocalDate();
-			chgDat.setText(Cst.CHANGED + changed.toString() + Cst.BY + chgusr);
+			chgDat.setText(CHANGED + changed.toString() + BY + chgusr);
 		}
 		if (deleteDate != null && deleteUser != null) {
 			LocalDate deleted = LocalDateTime.ofInstant(deleteDate, ZoneOffset.UTC).toLocalDate();
-			chgDat.setText(Cst.REMOVED + deleted.toString() + Cst.BY + deleteUser);
+			chgDat.setText(REMOVED + deleted.toString() + BY + deleteUser);
 		}
 
 	}
